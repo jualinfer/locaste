b64b659021569f7bb9c602b3b1808fbb2d16f8fe
Jose Daniel Solano Puech <josopu96@gmail.com>
2018-12-20 18:53:07 +0100

    Booth[Feat]:Now you can keep your session opened for a time

--- decide/booth_app/src/pages/home/home.ts
+++ decide/booth_app/src/pages/home/home.ts
@@ -1,3 +1,4 @@
+import { CookieService } from 'ngx-cookie-service';
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
@@ -7,8 +8,24 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  url: string = "..\\..\\assets\\imgs\\Pet.png";
+  showLoginPage: boolean = true;
 
+  constructor(
+    public navCtrl: NavController,
+    private cookieService: CookieService,
+  ) {
+    this.checkIfLogged();
+  }
+
+  private checkIfLogged() {
+    if(this.cookieService.get('decide')) {
+      this.showLoginPage = false;
+    }
+  }
+
+  public hiddeLogin($event?: boolean) {
+    this.showLoginPage = $event;
   }
 
 }
