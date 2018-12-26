0f4039ad4703441aca342705a47e8c11b385159c
Angel Marmol Fernandez <angelmarmolfernandez@gmail.com>
2018-12-26 21:04:46 +0100

    Auth[feat]: Voting with social accounts
    Now users who have logged in using any social account can vote
    
    Closes #19

--- decide/authentication/views.py
+++ decide/authentication/views.py
@@ -10,9 +10,10 @@ from .serializers import UserSerializer
 
 class GetUserView(APIView):
     def post(self, request):
-        key = request.data.get('token', '')
-        tk = get_object_or_404(Token, key=key)
-        return Response(UserSerializer(tk.user, many=False).data)
+        user = None
+        if request.user.is_authenticated:
+            user = request.user
+        return Response(UserSerializer(user, many=False).data)
 
 
 class LogoutView(TemplateView):
