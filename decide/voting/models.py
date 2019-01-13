44f65e5fc9cf7f7d1d627e1d87541c1becc58713
Alejandro <alexgue404@gmail.com>
2019-01-13 12:38:08 +0100

    Add Image header field in model and views

--- decide/voting/models.py
+++ decide/voting/models.py
@@ -45,6 +45,7 @@ class QuestionOption(models.Model):
 class Voting(models.Model):
     name = models.CharField(max_length=200)
     desc = models.TextField(blank=True, null=True)
+    image_header = models.CharField(max_length=200)
     question = models.ManyToManyField(Question, related_name='voting_questions')
 
     start_date = models.DateTimeField(blank=True, null=True)
