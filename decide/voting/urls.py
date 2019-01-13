fc1f3c9d6790caec39616b3c5d446e3e1807f5f5
Manuel J <mrmj1996@gmail.com>
2019-01-13 21:58:10 +0100

    Voting[fix]: Fixed url bug affecting tests
    Removed unused url in voting urls

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.VotingView.as_view(), name='voting'),
-    path('<int:voting_id>/', views.VotingView.as_view(), name='voting'),
     path('create/', views.votingForm, name='create_voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
     path('list/', views.VotingList.as_view(), name='voting_list'),
