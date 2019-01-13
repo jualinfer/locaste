a46855a3ee0d0f4a350ac8af2d9d84fd50d1ade9
Daniel Diment <danieldiment.gj@gmail.com>
2019-01-13 13:59:14 +0100

    Voting[fix]: Added --nosandbox-mode to selenium test

--- decide/voting/tests.py
+++ decide/voting/tests.py
@@ -28,6 +28,7 @@ class VotingTestCaseSelenium(BaseTestCase):
         chrome_options = Options()
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--no-sandbox mode")
         # self.selenium = webdriver.Chrome('C:/Users/Daniel Diment/Downloads/chromedriver_win32/chromedriver.exe',chrome_options=chrome_options)
         self.selenium = webdriver.Chrome('/usr/local/bin/chromedriver',chrome_options=chrome_options)
         super().setUp()
